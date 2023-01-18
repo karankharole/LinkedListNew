89a9a98ada1aae39caccb0cf24319f0589929ac8
karankharole <karankharole2000@gmail.com>
2023-01-18 18:41:24 +0530

    create simple linked list and insert ekements

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace LinkedListNew
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            LinkedList l_Object = new LinkedList();
+            l_Object.add(56);
+            l_Object.add(30);
+            l_Object.add(70);
+            l_Object.display();
+        }
+    }
+}
