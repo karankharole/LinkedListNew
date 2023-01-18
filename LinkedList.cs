eb694ea25d92c46eb19bd0ae3865a03637da9e3a
karankharole <karankharole2000@gmail.com>
2023-01-18 22:03:15 +0530

    dleting last element in list

--- LinkedList.cs
+++ LinkedList.cs
@@ -86,5 +86,27 @@ namespace LinkedListNew
             head = head.next;
             return head;
         }
+
+        internal Node RemoveLastNode()
+        {
+            Console.WriteLine("Deleted last Element : ");
+
+            if (head == null)
+            {
+                return null;
+            }
+            if (head.next == null)
+            {
+                return null;
+            }
+            Node newNode = head;
+            while (newNode.next.next != null)
+            {
+                newNode = newNode.next;
+            }
+            newNode.next = null;
+            return head;
+        }
+
     }
 }
--- Program.cs
+++ Program.cs
@@ -8,12 +8,11 @@ namespace LinkedListNew
            
         {
             LinkedList l_Object = new LinkedList();
+
             l_Object.add(56);
+            l_Object.add(30);
             l_Object.add(70);
-            l_Object.display();
-            l_Object.InsertAtParticularPosition(2, 30);
-            l_Object.display();
-            l_Object.RemoveFirstNode();
+            l_Object.RemoveLastNode();
             l_Object.display();
         }
     }
